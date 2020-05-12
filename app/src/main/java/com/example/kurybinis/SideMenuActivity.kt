ffd17c2ac193a28c4efdcda0f46492b5d8805774
ignazzz <ignas.vaic83@gmail.com>
2020-05-12 20:31:56 +0300

    backBtn

--- app/src/main/java/com/example/kurybinis/SideMenuActivity.kt
+++ app/src/main/java/com/example/kurybinis/SideMenuActivity.kt
@@ -3,10 +3,18 @@ package com.example.kurybinis
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class SideMenuActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.side_menu)
+
+       val backBtn = findViewById<Button>(R.id.back)
+        backBtn.setOnClickListener {
+            finish();
+        }
+
     }
 }

a0e07f2dccb0eedf17cd03fbcd7d96d0698daf81
neringaaa <21.neringa.majauskaite@azuolynogimnazija.lt>
2020-05-12 19:56:10 +0300

    start new activity

--- app/src/main/java/com/example/kurybinis/MainActivity.kt
+++ app/src/main/java/com/example/kurybinis/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.example.kurybinis
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.BinderThread
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        open.setOnClickListener {
+            startActivity(Intent(this, SideMenuActivity::class.java))
+        }
+        }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        setContentView(R.layout.activity_main)
+
+        open.setOnClickListener {
+            openContextMenu(side_menu)
+        }
     }
-}
+
+    }
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/kurybinis/SideMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kurybinis
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class SideMenuActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.side_menu)
+    }
+}

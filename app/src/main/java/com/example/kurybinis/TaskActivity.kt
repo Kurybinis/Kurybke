88ee3c3b372ec8aa7515d540a31c5ca2875631ef
neringaaa <21.neringa.majauskaite@azuolynogimnazija.lt>
2020-05-28 19:43:50 +0300

    icona 0.1

--- app/src/main/java/com/example/kurybinis/TaskActivity.kt
+++ app/src/main/java/com/example/kurybinis/TaskActivity.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.listview.*
 class TaskActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.task)
+        setContentView(R.layout.prideti_uzd)
 
     }
 }
